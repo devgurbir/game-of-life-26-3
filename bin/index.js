8389fb48fc9723a737ae819a1e622a2cad6dd7d2
Gurbir <devgurbir@gmail.com>
2022-03-18 19:38:14 +0530

    #2, thinking + reading about the problem

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 
+// Initially creating the matrix
 let matrix = [];
-
 for(let i = 0; i<16; i++){
-    let temp = new Array(16).fill( Math.random() > 0.5 ? "LIVE" : "DEAD")
+    let temp = new Array(16)
     matrix.push(temp)
 }
 
-console.log(matrix)
+// populating matrix randomly with live/dead
+
+for(let i = 0; i<matrix.length; i++){
+    for(let j = 0; j<matrix[i].length; j++){
+        matrix[i][j] = Math.random() > 0.5 ? "LIVE" : "DEAD"
+    }
+}
+
+// go through each element and update the element according to rules of the game
+function goThroughMatrix(matrix){
+
+}
