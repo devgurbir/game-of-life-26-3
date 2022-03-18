fe9b785b42a27a49cd6f06cb6b5c93d4686c0b12
Gurbir <devgurbir@gmail.com>
2022-03-18 19:48:58 +0530

    #3, tryin to come up with an approach to traverse every element, seems like a dfs approach

--- bin/index.js
+++ bin/index.js
@@ -11,11 +11,30 @@ for(let i = 0; i<16; i++){
 
 for(let i = 0; i<matrix.length; i++){
     for(let j = 0; j<matrix[i].length; j++){
-        matrix[i][j] = Math.random() > 0.5 ? "LIVE" : "DEAD"
+        matrix[i][j] = Math.random() > 0.5 ? 0 : 1
     }
 }
 
-// go through each element and update the element according to rules of the game
+console.log(matrix)
+
+
 function goThroughMatrix(matrix){
+    // go through each element and update the element according to rules of the game    
+    for(let i = 0; i<matrix.length; i++){
+        for(let j = 0; j<matrix[i].length; j++){
+            if(matrix[i][j] == 1){
+
+            }
+        }
+    }
+}
+
+function checkLiveElement(matrix, i, j){
+    // rules of the game:
+
+    // if less than 2 living neighbours, die.
+}
+
+function checkNeighbours(matrix, i, j){
 
 }
