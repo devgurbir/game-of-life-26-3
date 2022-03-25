a6c849d3ca4d3752d5ac1bbb13cb4d30dd466ff9
Gurbir <devgurbir@gmail.com>
2022-03-26 05:12:59 +0530

    completed tests for blinker, toad, block

--- bin/index.test.js
+++ bin/index.test.js
@@ -29,3 +29,65 @@ test("Testing blinker 2", () => {
     [0, 1, 0],
   ]);
 });
+
+let currentForBlock = [
+  [0, 0, 0, 0],
+  [0, 1, 1, 0],
+  [0, 1, 1, 0],
+  [0, 0, 0, 0],
+];
+
+let nextForBlock = [[], [], [], []];
+
+test("Testing block", () => {
+  expect(goThroughMatrix(currentForBlock, nextForBlock, 0, 1)).toEqual([
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+  ]);
+});
+
+let currentToad1 = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+
+let nextToad1 = [[], [], [], [], [], []];
+
+test("Testing toad 1", () => {
+  expect(goThroughMatrix(currentToad1, nextToad1, 0, 1)).toEqual([
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+let currentToad2 = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+
+let nextToad2 = [[], [], [], [], [], []];
+
+test("Testing toad 2", () => {
+  expect(goThroughMatrix(currentToad2, nextToad2, 0, 1)).toEqual([
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ]);
+});
